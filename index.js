c53ff4c2ebad3f4ecea1f8145276e70d1daaeb70
kadirkayas <46502681+kadirkayas@users.noreply.github.com>
2024-04-24 21:25:50 +0900

    1.0

--- index.js
+++ index.js
@@ -1,68 +1,76 @@
-$(".start").click(function () {
-    start();    
-    $(this).text("Restart Game");
-})
-
-function start() {
-    
     let level = 0;
-    let colours = ["green", "red", "yellow", "blue"];
     let clickedColors = [];
     let gameColors = []
+$(".record").text("Max Record: "+getCookie("record"));
+$(".start").click(function () {
+    $(this).text("Restart Game");
+    if(getCookie("record")===null){
+        setCookie("record", 0);
+    }
+    clickedColors = [];
+    gameColors = []
+    level = 0;
     
     ramdomColor(); 
-    clickColorButton();
+})
+$(".click").click(function () {
+    let clicked=$(this).attr("id");
+    $("#" + clicked).addClass("pressed");
+    setTimeout(function() {
+        $("#" + clicked).removeClass("pressed");
+    }, 100);
+    clickedColors.push(clicked);
+    checkColor(clickedColors.length-1)
+})
 
-    function checkColor(clicked){
-        if(gameColors[clicked]===clickedColors[clicked]){
-            if(clickedColors.length===gameColors.length){
-                const audio = new Audio("sounds/level-up.mp3");
-                audio.play();
-                setTimeout(function() {
-                   ramdomColor();
-                }, 500);
-            }
-        }
-        else{
-            $(".title").text("Game Over");
-            startOver();
-        }
-    }
-    function startOver() {
-        level = 0;
-        gamePattern = [];
-        $("#start-btn").text("Start Game");
-        
-        const audio = new Audio("sounds/game-over.mp3");
-        audio.play();
-    }
-
-
-    function clickColorButton() {    
-        $(".click").click(function () {
-            let clicked=$(this).attr("id");
-            $("#" + clicked).addClass("pressed");
+function checkColor(clicked){
+    if(gameColors[clicked]===clickedColors[clicked]){
+        if(clickedColors.length===gameColors.length){
+            const audio = new Audio("sounds/level-up.mp3");
+            audio.play();
             setTimeout(function() {
-                $("#" + clicked).removeClass("pressed");
-            }, 100);
-            clickedColors.push(clicked);
-            console.log(clicked)
-            checkColor(clickedColors.length-1)
-
-         })
+               ramdomColor();
+            }, 700);
+        }
     }
-
-    function ramdomColor() {
-        level++;
-        clickedColors = [];
-        $(".title").text("Level " + level);
-        let colours = ["green", "red", "yellow", "blue"];
-        let random = Math.floor(Math.random() * 4);
-        gameColors.push(colours[random]);
-        $("#" + colours[random]).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-        console.log(colours[random]+" sonuc");
+    else{
+        $(".title").text("Game Over");
+        startOver();
     }
-    
+}
+function startOver() {
+    if(getCookie("record")<level){
+        setCookie("record", level);
+        $(".record").text("Max Record: "+getCookie("record"));
+    }
+    $("#start-btn").text("Start Game");
+    const audio = new Audio("sounds/game-over.mp3");
+    audio.play();
 }
 
 
+function ramdomColor() {
+    level++;
+    clickedColors = [];
+    $(".title").text("Level " + level);
+    let colours = ["green", "red", "yellow", "blue"];
+    let random = Math.floor(Math.random() * 4);
+    gameColors.push(colours[random]);
+    $("#" + colours[random]).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+}
+
+function setCookie(record, value) {
+    document.cookie = record + "=" + value ;
+}
+function getCookie(record) {
+    var recordNQ = record + "=";
+    var split = document.cookie.split(';');
+    for(var i=0;i < split.length;i++) {
+      var c = split[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(recordNQ) == 0) return c.substring(recordNQ.length,c.length);
+    }
+    return null;
+}
+
+  
